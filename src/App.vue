95a0b8fc251c8f7868fa2628fcbf16a9570ebec5
molincheng <1030658764@qq.com>
2022-12-07 01:31:00 +0800

    test-vue

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <div class="app">
+      <h2>app</h2>
+      <button @click="send" >给学生发送一份信</button>
+    </div>
+    <studentApp v-if="isShow" ></studentApp>
+    <button @click="isShow=!isShow">是否显示学生</button>
+  </div>
+</template>
+
+<script>
+import studentApp from './components/Student.vue'
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      isShow:true
+    }
+  },
+  components: {
+    studentApp
+  },
+  methods:{
+    send(){
+      this.$bus.$emit('fun1',{name:'lisi',age:22})
+    }
+  }
+}
+</script>
+
+<style>
+.app{
+  width:350px;
+  height:80px;
+  background:red;
+}
+</style>
---
+++ src/components/Student.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <ul>
+      <li v-for="(item,index) in list" :key="index" >{{item.name}} -- {{item.age}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'studentApp',
+  data(){
+    return{
+      list:[]
+    }
+  },
+  mounted(){
+    this.$bus.$on('fun1',obj=>{
+      this.list.push(obj)
+      console.log(obj)
+    })
+  },
+  beforeDestroy(){
+    console.log('student组件销毁了')
+    this.$bus.$off('fun1')
+  }
+}
+</script>
+
+<style>
+.hello{
+  margin-top:20px;
+  width:350px;
+  background:pink;
+}
+</style>
